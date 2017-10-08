284fcd5a6bb407dc1469b836acd4474f2f40af42
Florent Delmotte <delmotteflorent@sfr.fr>
2017-10-08 11:07:39 +0200

    passage spring-boot 1.5.7

--- src/main/java/fr/sport/muscu/api/Config.java
+++ src/main/java/fr/sport/muscu/api/Config.java
@@ -1,12 +1,5 @@
 package fr.sport.muscu.api;
 
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import fr.df.muscu.api.web.v1.ExopredefController;
-
 public interface Config {
 
 }
