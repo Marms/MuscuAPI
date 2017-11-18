c22bb8e765d001bd8b80c59f02109ed186b778d1
Florent Delmotte <delmotteflorent@sfr.fr>
2017-11-18 10:05:34 +0100

    Revert "passage spring-boot 1.5.7"
    This reverts commit 284fcd5a6bb407dc1469b836acd4474f2f40af42.

--- src/main/java/fr/sport/muscu/api/Main.java
+++ src/main/java/fr/sport/muscu/api/Main.java
@@ -2,8 +2,9 @@ package fr.sport.muscu.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
